01850fb69c15a0ba0494b09b2ab05e6cce501c80
wladimirgrf <wladimir.code@gmail.com>
2023-05-14 15:35:44 -0300

    feat: orgs repository, autenticate use case

---
+++ src/repositories/orgsRepository.ts
@@ -0,0 +1,7 @@
+import { Prisma, Org } from '@prisma/client'
+
+export interface OrgsRepository {
+  create(data: Prisma.OrgCreateInput): Promise<Org>
+  findByEmail(email: string): Promise<Org | null>
+  findById(id: string): Promise<Org | null>
+}
---
+++ src/useCases/authenticate.ts
@@ -0,0 +1,38 @@
+import { OrgsRepository } from '@/repositories/orgsRepository'
+import { Org } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalidCredentialsError'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  org: Org
+}
+
+export class AuthenticateUseCase {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, org.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      org,
+    }
+  }
+}
